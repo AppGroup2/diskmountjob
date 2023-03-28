44ad93f678fb56e18c871e4c8b6818ad4d24c2b4
teeto815 <smartteeto@gmail.com>
2023-03-27 23:35:25 -0700

    will use this file to practice pull request and approval method

---
+++ diskmount2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script will be use to mount extra disk volume
+# to our Jenkins server . Each member will write a line of 
+#code and we shall run it.
+#This will also help us work around our pull request, approval and
+#merging of codes in github.
+lsblk
+sudo mkdir /mnt/group2test
+
+
